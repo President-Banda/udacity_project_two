5b0d2631886d5b2031e87fe5c207b0a0a78e8f2f
president <abanda435@gmail.com>
2022-09-05 21:51:33 +0200

    implemented image fetching function

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Request, Router, Response} from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -30,7 +31,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get('/filteredimage', async(req: Request, res: Response) => {
+    const image_url = req.query.image_url.toString();
+    if(!image_url){
+      res.status(404).send('Image url is required');
+    }
+
+    const filteredimage = await filterImageFromURL(image_url);
+
+    res.status(200).sendFile(filteredimage, () => {
+      deleteLocalFiles([filteredimage]);
+    })
+    
+  })
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
