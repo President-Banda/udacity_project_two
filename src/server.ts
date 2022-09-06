4d5130571057d636112d17125aebdf65148528ff
president <abanda435@gmail.com>
2022-09-06 11:18:52 +0200

    fixed the string parameter requirment for type setting for the image-url variable

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   //! END @TODO1
   app.get('/filteredimage', async(req: Request, res: Response) => {
-    const image_url = req.query.image_url.toString();
+    const image_url :string = req.query.image_url.toString();
     if(!image_url){
       res.status(404).send('Image url is required');
     }
